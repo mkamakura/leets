59aa399d950fe601685e58b7ea66f985a2beeafa
mkamakura <kamakura.masaya@gmail.com>
2021-03-06 23:20:32 -0500

    feat(two-sum): update

--- two-sum/solution.go
+++ two-sum/solution.go
@@ -1,5 +1,5 @@
 func twoSum(nums []int, target int) []int {
-	for i := 0; i < len(nums); i++ {
+	for i := 0; i < len(nums) - 1; i++ {
 		for j := i + 1; j < len(nums); j++ {
 			sum := nums[i] + nums[j]
 			if sum == target {
